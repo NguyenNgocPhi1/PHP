8a397c49c4d1ed52a72da22ca6cc685a3e683247
unknown <you@example.com>
2022-12-20 16:17:03 +0700

    sua

---
+++ Chương _2/mang1.php
@@ -0,0 +1,10 @@
+<?php
+    //Mảng có chỉ số là số nguyên: 0,1,2,... Có 2 cách khai báo và sử dụng
+    // Cách 1:
+    $arr1 = arr(); //Khởi tạo mảng rỗng
+    $arr1[0] = 5; // Gán giá trị cho phần tử thứ 0
+    $arr1[1] = "Hello"; // Gán giá trị cho phần tử thứ 1
+    $arr1[2] = "PHP"; // Gán giá trị cho phần tử thứ 2
+    $arr1[3] = 15; // Gán giá trị cho phần tử thứ 3
+    var_dump($arr1);
+?>
